a4eabc9838f0a3a3a257679e8f6418ae1f264c01
Venkat Naidu <naiduv@bittorrent.com>
2012-12-30 20:05:11 -0800

    wip

---
+++ MvcApplication2/MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "White Marketing";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us";
+
+            return View();
+        }
+
+        public ActionResult Management()
+        {
+            ViewBag.Message = "Management";
+
+            return View();
+        }
+    }
+}
